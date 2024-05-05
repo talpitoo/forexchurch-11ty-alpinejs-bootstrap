15dbad5fa3c40161213bb9a2e9e929867f64d8a1
talpitoo <mounttain@gmail.com>
2024-05-05 19:23:09 +0200

    popover content

--- src/js/modules/popoverCustom.js
+++ src/js/modules/popoverCustom.js
@@ -2,7 +2,7 @@ export function popoverCustom() {
     return {
         openPopover: false,
         popperInstance: null,
-        
+
         init() {
             this.popperInstance = createPopper(this.$refs.button, this.$refs.popoverCustom, {
                 modifiers: [
@@ -14,16 +14,34 @@ export function popoverCustom() {
                     },
                 ],
             });
+
+            // Setup global click handler to close popover when clicking outside
+            document.addEventListener('click', (event) => {
+                // Check if the click is outside the popover and the button
+                if (!this.$refs.popoverCustom.contains(event.target) && !this.$refs.button.contains(event.target)) {
+                    this.closePopover();
+                }
+            });
         },
 
         togglePopover() {
             this.openPopover = !this.openPopover;
+            this.updatePopoverDisplay();
+        },
+
+        closePopover() {
+            this.openPopover = false;
+            this.updatePopoverDisplay();
+        },
+
+        updatePopoverDisplay() {
             if (this.openPopover) {
-                this.popperInstance.update();
                 this.$refs.popoverCustom.style.display = 'block';
+                this.popperInstance.update();
             } else {
                 this.$refs.popoverCustom.style.display = 'none';
             }
         }
+
     };
 }
